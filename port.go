fecf979fe1ec0075733cd13df3b805fb125cb01d
chochy2001 <ohchochy@gmail.com>
2024-04-28 20:08:03 -0600

    Se hizo de manera concurrente el codigo de escaneo de puertos

--- port.go
+++ port.go
@@ -3,21 +3,28 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
-	for i := 0; i < 100; i++ {
+	var wg sync.WaitGroup
+	for i := 0; i < 65535; i++ {
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
 
-		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
+			if err != nil {
+				return
+			}
+			errCon := conn.Close()
+			if errCon != nil {
+				return
+			}
+			fmt.Printf("El puerto %d esta abierto\n", i)
 
-		if err != nil {
-			continue
-		}
-		errCon := conn.Close()
-		if errCon != nil {
-			return
-		}
-		fmt.Printf("El puerto %d esta abierto", i)
+		}(i)
 	}
-	fmt.Println("vim-go")
+	wg.Wait()
+	fmt.Println("Puertos escaneados con exito")
 }
