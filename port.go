5a67512425a523943440c7a62e1697501d73c2d7
chochy2001 <ohchochy@gmail.com>
2024-04-28 20:13:47 -0600

    Se agrego un flag para poder determinar el sitio web a escanear

--- port.go
+++ port.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"sync"
 )
 
+var site = flag.String("site", "scanme.nmap.org", "Site to scan")
+
 func main() {
+	flag.Parse()
 	var wg sync.WaitGroup
 	for i := 0; i < 65535; i++ {
 		wg.Add(1)
 		go func(port int) {
 			defer wg.Done()
-			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *site, port))
 
 			if err != nil {
 				return
